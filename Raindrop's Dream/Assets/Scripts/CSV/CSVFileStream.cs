36eb7b40b515c02469c2df3f5da574d5cbc1b1f3
AICHEN <764668210@qq.com>
2018-05-28 22:26:57 +0800

    CSV读取工具-1

---
+++ Raindrop's Dream/Assets/Scripts/CSV/CSVFileStream.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVFileStream
+{
+    public static Dictionary<string, Dictionary<string, string>> ReadCsvFile(string _fileName)
+    {
+        Dictionary<string, Dictionary<string, string>> result = new Dictionary<string, Dictionary<string, string>>();
+        string url = Application.streamingAssetsPath + "/" + _fileName;
+       
+        string[] fileData = File.ReadAllLines(url);
+
+        if (fileData.Length < 3)
+        {
+            return result;
+        }
+
+        /* CSV文件的第一行为Key字段，第二行为说明（不需要读取），第三行开始是数据。第一个字段一定是ID。 */
+        string[] keys = fileData[0].Split(',');
+        for (int i = 2; i < fileData.Length; i++)
+        {
+            string[] line = fileData[i].Split(',');
+
+            /* 以ID为key值，创建一个新的集合，用于保存当前行的数据 */
+            string ID = line[0];
+            result[ID] = new Dictionary<string, string>();
+            for (int j = 0; j < line.Length; j++)
+            {
+                /* 每一行的数据存储规则：Key字段-Value值 */
+                result[ID][keys[j]] = line[j];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Raindrop's Dream/Assets/Scripts/CSV/PublicDataManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+public static class PublicDataManager
+{
+   private static  Dictionary<Type, object> items;
+   public 
+}
