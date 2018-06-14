5ecd0370c3dd34079d1cddc3694a079a007af083
zhaoce <zhaoce@outlook.com>
2018-06-14 21:42:06 +0800

    添加一个全局的path拼接函数

---
+++ Raindrop's Dream/Assets/Scripts/Common/RD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RD
+{
+    public static bool IsOSXEditor = Application.platform == RuntimePlatform.OSXEditor;
+
+    public static bool IsWindowsEditor = Application.platform == RuntimePlatform.WindowsEditor;
+
+    //拼接路径
+    public static string SplitPath(string[] paths){
+
+        string split_character = IsOSXEditor ? "/" : "\\";
+
+        string path = paths[0];
+
+        for (var i = 1; i < paths.Length; i++){
+            path += split_character + paths[i];
+        }
+
+        return path;
+    }
+}
